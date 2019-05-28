f0d0d20efe940727f07d63bf611fb1c28c1906c8
alois-trancy <hagaren_renkin@yahoo.com>
2019-05-28 16:29:08 +0800

    Redirect to threads if user try to visit create new thread page

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 // Route::resource('threads', 'ThreadsController');
 Route::get('/threads', 'ThreadsController@index')->name('threads');
-Route::get('/threads/create', 'ThreadsController@create');
+Route::get('/threads/create', 'ThreadsController@create')->middleware('must-be-confirmed');;
 Route::get('/threads/search', 'SearchController@show');
 Route::post('/threads', 'ThreadsController@store')->middleware('must-be-confirmed');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
